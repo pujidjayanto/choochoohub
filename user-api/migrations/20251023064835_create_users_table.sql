2ef76c8bfda0afae552dd525b743724dde40aaed
pujidjayanto <dedypuji@outlook.com>
2025-10-23 15:04:11 +0700

    feat: add new field on users

--- user-api/migrations/20251023064835_create_users_table.sql
+++ user-api/migrations/20251023064835_create_users_table.sql
@@ -2,21 +2,24 @@
 -- +goose StatementBegin
 create table users (
   id uuid primary key,
-  email text not null unique,
+  email varchar(255) not null unique,
   password text not null,
   phone varchar(20) not null unique,
-  name text not null,
+  name varchar(255) not null,
+  dob date,
+  gender char(1) not null,
+  identity_number varchar(50),
+  identity_type varchar(20),
   created_at timestamptz not null,
   updated_at timestamptz not null
 );
 
 create index idx_users_name on users (name);
 create index idx_users_phone on users (phone);
+create index idx_users_identity_number on users (identity_number);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop index if exists idx_users_name;
-drop index if exists idx_users_phone;
 drop table if exists users;
 -- +goose StatementEnd
