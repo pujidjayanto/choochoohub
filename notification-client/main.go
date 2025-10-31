f4f824248b4c228c9a79ffba0971198c2893e82c
pujidjayanto <dedypuji@outlook.com>
2025-11-01 06:48:32 +0700

    feat: create dummy notification client

---
+++ notification-client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "otp.created",
+		GroupID: "otp-consumer-group",
+	})
+
+	defer reader.Close()
+
+	fmt.Println("Listening for messages on topic otp.created...")
+
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Received message: %s\n", string(m.Value))
+	}
+}
