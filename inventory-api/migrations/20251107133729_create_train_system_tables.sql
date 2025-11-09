0e47ec62d8017424000dce43c1b25b308250245d
pujidjayanto <dedypuji@outlook.com>
2025-11-09 19:57:29 +0700

    feat: v2 trains schema

--- inventory-api/migrations/20251107133729_create_train_system_tables.sql
+++ inventory-api/migrations/20251107133729_create_train_system_tables.sql
@@ -19,26 +19,41 @@ CREATE TABLE trains (
   class_code VARCHAR(10) NOT NULL, -- ECO, EXE, BIS
   subclass_name VARCHAR(100), -- AC, Non-AC, Premium, etc.
   subclass_code VARCHAR(20),
+  direction VARCHAR(100) NOT NULL, -- Surabaya - Malang
   created_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE train_stops (
+CREATE TABLE train_routes (
   id UUID PRIMARY KEY,
   train_id INT REFERENCES trains(id) ON DELETE CASCADE,
   station_id INT REFERENCES stations(id),
-  stop_order INT NOT NULL,          -- e.g. 1, 2, 3, ...
+  route_order INT NOT NULL,          -- e.g. 1, 2, 3, ...
   arrival_time TIME,                -- nullable for first station
   departure_time TIME,              -- nullable for last station
+  cumulative_price NUMERIC(12,2) NOT NULL,
+  distance_km NUMERIC(8,2) NOT NULL DEFAULT 0, -- distance from previous station in km
   created_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
-  UNIQUE(train_id, stop_order)
+  UNIQUE(train_id, route_order)
 );
+
+CREATE TABLE train_schedules (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  train_id UUID NOT NULL REFERENCES trains(id) ON DELETE CASCADE,
+  departure_station_id UUID NOT NULL REFERENCES stations(id),
+  destination_station_id UUID NOT NULL REFERENCES stations(id),
+  schedule_date DATE NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS train_stops;
+DROP TABLE IF EXISTS train_schedules;
+DROP TABLE IF EXISTS train_routes;
 DROP TABLE IF EXISTS trains;
 DROP TABLE IF EXISTS stations;
 -- +goose StatementEnd
